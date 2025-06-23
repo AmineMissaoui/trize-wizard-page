c6332deb03cf0ed11787b922b8391ec26877ac90
AmineMissaoui <amine.missaoui@esprit.tn>
2025-06-23 16:14:08 -0400

    feat: implement-yup-controle-for-assetInfo

--- app/wizard/steps/AssetInfo.tsx
+++ app/wizard/steps/AssetInfo.tsx
@@ -1,20 +1,47 @@
-import { Box, TextField, Typography } from '@mui/material';
+import { Box, TextField } from "@mui/material";
+import { useFormContext } from "react-hook-form";
 
 export default function AssetInfo() {
-    return (
-        <Box key="asset">
-            <h2>Step 1: Asset Info</h2>
-            <div className='row'>
-                <div className='col'>
-                    <TextField fullWidth label="Asset Symbol" variant="outlined" />
-                </div>
-                <div className='col'>
-                    <TextField fullWidth label="Asset Name" variant="outlined" />
-                </div>
-                <div className='col'>
-                    <TextField fullWidth label="Asset Decimals" variant="outlined" />
-                </div>
-            </div>
-        </Box>
-    );
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <Box key="asset">
+      <h2>Step 1: Asset Info</h2>
+      <div className="row">
+        <div className="col">
+          <TextField
+            fullWidth
+            label="Asset Symbol"
+            variant="outlined"
+            {...register("assetSymbol")}
+            error={!!errors.assetSymbol}
+            helperText={errors.assetSymbol?.message?.toString()}
+          />
+        </div>
+        <div className="col">
+          <TextField
+            fullWidth
+            label="Asset Name"
+            variant="outlined"
+            {...register("assetName")}
+            error={!!errors.assetName}
+            helperText={errors.assetName?.message?.toString()}
+          />
+        </div>
+        <div className="col">
+          <TextField
+            fullWidth
+            label="Asset Decimals"
+            variant="outlined"
+            {...register("decimals")}
+            error={!!errors.decimals}
+            helperText={errors.decimals?.message?.toString()}
+          />
+        </div>
+      </div>
+    </Box>
+  );
 }
